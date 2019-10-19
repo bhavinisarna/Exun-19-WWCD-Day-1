539b9f3a7f75233892bf455a30f07346bec5505b
bhavinisarna <56750720+bhavinisarna@users.noreply.github.com>
2019-10-19 13:27:02 +0530

    Added code for palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,13 @@
 # Given a number n, check whether it is a palindrome or not.
+# Given a number n, check whether it is a palindrome or not.
+n=int(input("enter a number"))
+t=n
+rev=0
+while(n>0):
+    a=n%10
+    rev=rev*10+a
+    n=n//10
+if (t==rev):
+    print("this is a palindrome")
+else:
+    print("this is not a palindrome")
